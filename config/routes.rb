64718942adb2b7c088a16f8fc4b47ff7a65e3068
Marie Lamoureux <lamoureuxmarie@outlook.com>
2022-08-17 12:51:59 +0200

    rails routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :restaurants
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # A visitor can see the list of all restaurants
+  # A visitor can add a new restaurant, and be redirected to the show view of that new restaurant.
+  # A visitor can see the details of a restaurant, with all the reviews related to the restaurant.
+  # A visitor can add a new review to a restaurant
+  resources :restaurants, only: [:index, :new, :create, :show] do
+    resources :reviews, only: [:index, :create]
 end
