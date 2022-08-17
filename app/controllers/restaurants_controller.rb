a1ef82409ba3f820ae77290d6ab09e77091fc60a
Marie Lamoureux <lamoureuxmarie@outlook.com>
2022-08-17 16:42:29 +0200

    added new restaurant and reviews forms

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,16 +1,32 @@
 class RestaurantsController < ApplicationController
-  before_action :set_restaurant, only: %i[show]
-
   def index
     @restaurants = Restaurant.all
   end
 
   def show
+    set_restaurant
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(set_params)
+    if @restaurant.save
+      redirect_to @restaurant, notice: 'Restaurant successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
 
   def set_restaurant
     @restaurant = Restaurant.find(params[:id])
+  end
+
+  def set_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
   end
 end
